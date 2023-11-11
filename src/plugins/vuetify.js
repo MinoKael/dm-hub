61e667c0febadac46b41e9d34292a5f3752a9233
Mikael Mota <mikael.mota@localize.com.br>
2023-11-11 18:27:00 -0300

    chore: arquivos removidos e libs instaladas

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,11 @@
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+export default createVuetify({
+  customVariables: [],
+  theme: {
+    defaultTheme: 'dark'
+  }
+})
---
+++ src/views/DiceView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <RouterLink to="/">
+      <v-btn>Home</v-btn>
+    </RouterLink>
+    <h1>This is an DICE page</h1>
+  </div>
+</template>
